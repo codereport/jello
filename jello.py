04b944b7525e677ff033c4c3937ddca0cbc5b46d
Conor Hoekstra <codereport@outlook.com>
2023-11-18 13:11:44 -0500

    :construction_worker: add ruff + fixes

--- jello.py
+++ jello.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import subprocess
-from colorama import init, Fore, Style
+
+from colorama import Fore, Style, init
+
 
 def cprint(s: str, c):
     print(Style.BRIGHT + c + s + Fore.RESET)
 
 def run_jelly(expr: str, arg: str):
     try:
-        command = ['jelly', 'eun', expr, arg]
+        command = ["jelly", "eun", expr, arg]
         result = subprocess.run(command, text=True, capture_output=True, check=True)
         output_text = result.stdout.strip()
 
@@ -20,19 +22,20 @@ def run_jelly(expr: str, arg: str):
         print(Fore.RED + "stderr:", e.stderr)
 
 def to_jelly(token: str) -> str:
-    if token == 'iota': return 'R'
-    if token == 'rev':  return 'Ṛ'
+    if token == "iota": return "R"
+    if token == "rev":  return "Ṛ"
+    return None
 
 def convert(expr: list[str]) -> str:
-    return ''.join([to_jelly(t) for t in expr])
+    return "".join([to_jelly(t) for t in expr])
 
 if __name__ == "__main__":
     init() # folor colorama
 
-    print('🟢🟡🔴 Jello 🔴🟡🟢\n')
+    print("🟢🟡🔴 Jello 🔴🟡🟢\n")
 
     while True:
-        expr = input('> ').strip().split()
+        expr = input("> ").strip().split()
         arg = expr[-1]
         converted_expr = convert(expr[:-1])
         cprint("   " + converted_expr, Fore.YELLOW)
